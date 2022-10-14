8d470178a84e2391e0a06642f4a94e7b8717847a
Kira Smiley <kira640@revature.net>
2022-10-14 15:30:48 -0400

    completed

--- WeeklyCodingChallenge(s)/src/WeekOfOct12/Main.java
+++ WeeklyCodingChallenge(s)/src/WeekOfOct12/Main.java
@@ -1,8 +1,11 @@
 package WeekOfOct12;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 
 public class Main {
 
@@ -14,11 +17,14 @@ public class Main {
 		
 		x.addAll(Arrays.asList("a", "b", "c", "d", "1", "2", "3"));
 		y.addAll(Arrays.asList("1", "2", "3", "a", "b", "c", "n"));
-		z.addAll(Arrays.asList("h", "i", "j", "k", "l", "m", "n"));
+		z.addAll(Arrays.asList("m", "i", "j", "k", "l", "m", "n"));
 		
 		System.out.println("Test 1: " + Intersection(x, y));
 		System.out.println("Test 2: " + Intersection(x, z));
 		System.out.println("Test 3: " + Intersection(y, z));
+		
+		System.out.println("\nTest 1: " + isCircle(z));
+		System.out.println("Test 2: " + isCircle(y));
 	}
 
 	public static String Intersection(LinkedList<String> one, LinkedList<String> two) {
@@ -35,5 +41,19 @@ public class Main {
 		
 		return "No ntersection found";
 
+	}
+	
+	public static String isCircle(LinkedList<String> one) {
+		
+		Set<String> x = new HashSet<>();
+		int y = one.size() + 1;
+		for(int i = 0; i < one.size(); i++) {
+			x.add(one.get(i));
+			if(x.size() != i + 1) {
+				y = i;
+				return "Circle found at node: " + one.get(y);
+			}
+		}
+		return "No circle found";
 	}
 	}
